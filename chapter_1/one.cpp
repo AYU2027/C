156ae529df5e52f9435f53e54b3951f27e88ddab
Ayush Sinha <ayush40451k@gmail.com>
2025-08-31 17:44:10 +0000

    n

--- chapter_1/one.cpp
+++ chapter_1/one.cpp
@@ -5,3 +5,9 @@ int main() {
     cout << "Hello World!";
     return 0;
 }
+
+
+// C++ is compilied based language 
+// we write -program code or source code-gives to complier then 
+//-- it convert it
+//  to the Object Code (savewith .obj extension separately) or machine code 
