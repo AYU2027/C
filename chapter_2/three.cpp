f4267d30697e15a74f505bb74331f79ce9024787
Ayush Sinha <ayush40451k@gmail.com>
2025-09-01 13:44:29 +0000

    ii

---
+++ chapter_2/three.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"types of the datatypes";
+    int age;
+    string chr;
+    double num;
+    float big;
+    char dd;
+    bool what;
+}
--- chapter_2/two.cpp
+++ chapter_2/two.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
+    // about testing the variable
+    string name;
+    int age = 34;
+    cin>>name>>age; // taking in the object 
+    cout<<name<<endl;
 
 };
