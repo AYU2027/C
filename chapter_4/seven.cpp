7dc07c8d9755156b0356339d4d09bb66f04cf733
Ayush Sinha <ayush40451k@gmail.com>
2025-09-16 17:18:10 +0000

    KK

---
+++ chapter_4/seven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cin>>a;
+    switch (a)
+    {
+        case 1: case 3: case 5:
+        cout<<"Print Odd";break;
+        case 2: case 4: case 6:
+        cout<<"print Even";break;
+         default: break;
+    
+   
+       
+    }
+    return 0;
+}
