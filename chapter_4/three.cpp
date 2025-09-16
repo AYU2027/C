d187b549ab5ab18accbd41df1e1c43153c70bf36
Ayush Sinha <ayush40451k@gmail.com>
2025-09-16 10:04:29 +0000

    m

---
+++ chapter_4/three.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 22;
+    int b = 3;
+    _Float16 c = 66;
+    float r = 8;
+    double tt = 89;
+    double t1 = 7;
+
+    cout<<a /6;
+          c/6;
+
+}
---
+++ chapter_4/two.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //  this way to define the constant in the programming 
+    constexpr int  a = 45;
+    const int v = 56;
+
+    cout<<a;
+
+}
