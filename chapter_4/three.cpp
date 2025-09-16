9f2175ea33823565e0c2a4cecaf2c92f29d1554b
Ayush Sinha <ayush40451k@gmail.com>
2025-09-16 10:07:52 +0000

    m

--- chapter_4/three.cpp
+++ chapter_4/three.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main(){
     int a = 22;
     int b = 3;
-    _Float16 c = 66;
+   
     float r = 8;
     double tt = 89;
     double t1 = 7;
 
     cout<<a /6;
-          c/6;
+    cout<<tt/5;
 
 }
