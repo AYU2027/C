f59210247bec3aa3f5df2312dd03323ec5ecf7cc
Ayush Sinha <ayush40451k@gmail.com>
2025-09-17 14:51:17 +0000

    17/9

---
+++ chapter_4/exercise_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a ;
+    int b;
+  
+    while(a =="|" or b == "|"){
+          cin>>a;
+    cin>>b;
+        cout<<a <<b;
+
+
+        
+    }
+}
+
+//  1. Write a program that consists of a while-loop that (each time around the loop) reads in two ints and then prints them.
+//  Exit the program when a terminating '|' is entered.
+//  2. Change the program to write out the smaller value is: followed by the smaller of the numbers and the larger value
+//  is: followed by the larger value.
+//  3. Augment the program so that it writes the line the numbers are equal (only) if they are equal.
+//  4. Change the program so that it uses doubles instead of ints.
