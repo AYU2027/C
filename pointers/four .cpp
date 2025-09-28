5891a7c004994bd7280940b07bdb03b2a5233444
Ayush Sinha <ayush40451k@gmail.com>
2025-09-28 09:49:10 +0000

    io

--- pointers/four .cpp
+++ pointers/four .cpp
@@ -3,7 +3,22 @@
 using namespace std ;
 
 int main(){
-    int arr[5];
+    int size;
+    cout<<"size: ";
+    cin>>size;
+    int* myArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Array[" << i <<"] ";
+        cin >> myArray[i];
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        cout<< myArray[i]<<"  ";
+    }
+    
 
     system("pause>0");
 }
