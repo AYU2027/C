215330d2753d2e2ac192e0f985822f6517386b95
Ayush Sinha <ayush40451k@gmail.com>
2025-09-28 09:19:31 +0000

    D

--- pointers/three.cpp
+++ pointers/three.cpp
@@ -19,14 +19,22 @@ int getmax(int numbers[],int size){
     return max;
 }
 
-// void getMinAndMax(int numbers[] int size,int* min ,int){
+void getMinAndMax(int numbers[], int size,int* min ,int* max){
+    for(int i = 1;i <size;i++){
+        if(numbers[i]>*max)
+          *max = numbers[i];
+        if(numbers[i]<*min)
+          *min = numbers[i];
+    }
     
-// }
+}
 
 int main(){
     int numbers[5] = {5,4,-2,29,6};
-    cout<<"Max is "<<getmax(numbers,5)<<endl;
-    cout<<"MIn is "<<getmin(numbers,5)<<endl;
-
+    // cout<<"Max is "<<getmax(numbers,5)<<endl;
+    // cout<<"MIn is "<<getmin(numbers,5)<<endl;
+    int min = numbers[0];
+    int max = numbers[0];
+    getMinAndMax(numbers,5,&min,&max);
 
 }
